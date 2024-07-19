032b37628f0db96cf1c98576e206ccf416c9cec3
Diones Pereira Alves <pereiradiones987@gmail.com>
2024-07-19 13:52:32 -0300

    configurações iniciais

---
+++ src/data/constants/produtos.ts
@@ -0,0 +1,39 @@
+import Produto from "../model/Produto";
+
+const produtos: Produto[] = [
+  {
+    id: 1,
+    nome: 'Camiseta',
+    descricao: 'Camiseta de algodão 100% com estampa exclusiva.',
+    preco: 49.99,
+    imagem: 'https://images.unsplash.com/photo-1512412046877-7b5c00e60a88'
+  },
+  {
+    id: 2,
+    nome: 'Calça Jeans',
+    descricao: 'Calça jeans confortável e estilosa.',
+    preco: 89.99,
+    imagem: 'https://images.unsplash.com/photo-1517669366364-4012441130d3'
+  },
+  {
+    id: 3,
+    nome: 'Tênis Esportivo',
+    descricao: 'Tênis esportivo leve e durável.',
+    preco: 119.99,
+    imagem: 'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f'
+  },
+  {
+    id: 4,
+    nome: 'Relógio de Pulso',
+    descricao: 'Relógio de pulso à prova d\'água.',
+    preco: 199.99,
+    imagem: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77'
+  },
+  {
+    id: 5,
+    nome: 'Mochila',
+    descricao: 'Mochila espaçosa com vários compartimentos.',
+    preco: 79.99,
+    imagem: 'https://images.unsplash.com/photo-1505682634904-d7c5d8c53acb'
+  }
+]
